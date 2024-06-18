1d82c41ab0c9d35cd5010e8b91cf28bab4e9f033
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-06-17 20:18:34 -0600

    listo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,11 +25,11 @@ const updateContactOpen = (value) => {
   <DrawerContact v-bind:contactOpen="contactOpen" @update:contactOpen="updateContactOpen" />
 
   <main>
-    <div id="default-carousel" class="relative w-full pt-16 " data-carousel="static">
+    <div id="default-carousel" class="relative w-full pt-16 " data-carousel="slide">
       <!-- Carousel wrapper -->
       <div class="relative h-96  overflow-hidden lg:h-dvh">
         <!-- Item 1 -->
-        <div class="bg-white hidden duration-700 ease-in-out" data-carousel-item>
+        <div class="bg-white hidden duration-500 ease-in-out" data-carousel-item>
           <div class="mx-auto max-w-7xl px-6 py-5 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-5">
             <div class="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
               <div class="flex">
@@ -133,7 +133,7 @@ const updateContactOpen = (value) => {
         data-carousel-prev>
         <span
           class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
+          <svg class="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M5 1 1 5l4 4" />
@@ -146,7 +146,7 @@ const updateContactOpen = (value) => {
         data-carousel-next>
         <span
           class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
+          <svg class="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="m1 9 4-4-4-4" />
