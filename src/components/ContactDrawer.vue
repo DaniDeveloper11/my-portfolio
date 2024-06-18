2a14216402c86f47da7f61e0d3e35d699c267765
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-06-17 20:37:10 -0600

    initial deploy

--- src/components/ContactDrawer.vue
+++ src/components/ContactDrawer.vue
@@ -10,7 +10,7 @@
                 <div class="flex items-center justify-between">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">Your Company</span>
-                        <img class="h-20 w-auto" src="./assets/logo2.png " alt="" />
+                        <img class="h-20 w-auto" src="/src/assets/portafolio.png" alt="" />
                     </a>
 
                 </div>
